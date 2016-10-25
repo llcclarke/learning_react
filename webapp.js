8d91910ae7dfb9b45cc597a8e8fe40d249ace2e8
lucy <l.l.c.clarke@gmail.com>
2016-10-25 17:10:31 +0200

    initial api state inputed

--- webapp.js
+++ webapp.js
@@ -4,6 +4,18 @@ var app = express();
 
 app.use(express.static('static'));
 
+// var jobData = [
+// 	{id: 1, status:'Complete', due:'Tomorrow', title:'Laundry', comments:''}
+// ];
+
+var jobData = [
+  {id: 1, status:'Complete', due:'Tomorrow', title:'Laundry', comments:''},
+  {id: 2, status:'To Do', due:'Next Week', title:'Book flights', comments:'under 200€'},
+];
+app.get('/api/jobs', function(req, res) {
+	res.status(200).send(JSON.stringify(jobData))
+});
+
 var server = app.listen(3000, function() {
 	var port = server.address().port;
 	console.log("Started server at port", port);
