0f5d5bf90c522273d721d3909e60b22756b2caf0
lucy <l.l.c.clarke@gmail.com>
2016-11-02 04:11:23 +0100

    created gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('bundle', function(){
+  return browserify('src/App.js')
+    .transform('babelify', {presets: 'react'})
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('static/'));
+})
