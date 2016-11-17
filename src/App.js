c6abfa97d5c2d853c8fedff7b6a563733bd6c387
lucy <l.l.c.clarke@gmail.com>
2016-11-17 22:33:34 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 'use strict'
 var React = require('react');
 var ReactDOM = require('react-dom');
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+
 
 var JobList = require('./JobList');
 
+var NoMatch = React.createClass({
+  render: function() {
+    return (
+      <h2>No match for the  route</h2>
+    );
+  }
+});
+
 ReactDOM.render(
-  <JobList />,
+  (
+    <div>
+      <Router history={hashHistory} >
+        <Route path="/jobs" component={JobList} />
+        <Redirect from="/" to="/jobs" />
+        <Route path="*" component={NoMatch} />
+        </Router>
+      </div>
+  ),
   document.getElementById('main')
 );
+//
+//
+// ReactDOM.render(
+//   <JobList />,
+//   document.getElementById('main')
+// );
